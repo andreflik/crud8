f091840748d2116bd56755b5cbe004f2c7ab0ed7
andreflik <andreflik@gmail.com>
2021-09-29 22:52:51 -0300

    crud8

---
+++ resources/views/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastrar novo Produto</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Nome</label>
+        <input type="text" name="nome">
+        <label for="">Custo</label>
+        <input type="text" name="custo">
+        <label for="">Preço</label>
+        <input type="text" name="preco">
+        <label for="">Quantidade</label>
+        <input type="text" name="quantidade">
+    </form>
+</body>
+</html>
